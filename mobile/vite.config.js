6b5cdca8a111390c079bf9d1dcda9067e06ffb34
facupetitfour <facupetitfour@gmail.com>
2025-04-25 12:24:58 -0300

    cambios para pwa

--- mobile/vite.config.js
+++ mobile/vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa'; // Correcta importación
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'WineClub',
+        short_name: 'WineClub',
+        description: 'Wine Club PWA App',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'favicon.ico',
+            sizes: '192x192',
+            type: 'image/x-icon',
+          },
+          {
+            src: 'favicon.ico',
+            sizes: '512x512',
+            type: 'image/x-icon',
+          },
+        ],
+      },
+    }),
+  ],
+});
